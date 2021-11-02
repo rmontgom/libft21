f689e194c2c214929508fd187640cdfb89eb83d8
rmontgom <rmontgom@student.21-school.ru>
2021-11-02 22:30:24 +0500

    first commit

---
+++ libft/ft_atoi.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmontgom <rmontgom@42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/12 19:58:33 by rmontgom          #+#    #+#             */
+/*   Updated: 2021/10/19 23:45:30 by rmontgom         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	nbr;
+	int	znak;
+
+	nbr = 0;
+	znak = 0;
+	i = 0;
+	while ((str[i] == '\n') || (str[i] == '\t') || (str[i] == '\v')
+		|| (str[i] == ' ') || (str[i] == '\f') || (str[i] == '\r'))
+		i++;
+	if (str[i] == '-')
+		znak = 1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	while (str[i] && (str[i] >= '0') && (str[i] <= '9'))
+	{
+		nbr *= 10;
+		nbr += (int)str[i] - '0';
+		i++;
+	}
+	if (znak == 1)
+		return (-nbr);
+	else
+		return (nbr);
+}
+
+/*int	main()
+{
+	printf("%d", ft_atoi("9223abc"));
+	printf("%d", atoi("9223abc"));
+	return(0);
+}*/
